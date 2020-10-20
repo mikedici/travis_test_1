59cc484df609e78f80c538599e94a426cdda97f7
Michael DiCicco <mdicicco@uaex.edu>
2020-10-19 21:04:21 -0500

    Update tests.py

--- junk/tests.py
+++ junk/tests.py
@@ -6,3 +6,22 @@ class BasicTest(TestCase):
     def test_basic_case(self):
         print("here goes")
         self.assertTrue(True, "This shouldn't ever fail")
+
+
+# Create your tests here.
+class BasicTestTwo(TestCase):
+    def test_basic_case(self):
+        print("here goes")
+        self.assertTrue(True, "This shouldn't ever fail")
+
+
+class BasicTestThree(TestCase):
+    def test_basic_case(self):
+        print("here goes")
+        self.assertTrue(True, "This shouldn't ever fail")
+
+
+class BasicTestFour(TestCase):
+    def test_basic_case(self):
+        print("here goes")
+        self.assertTrue(True, "This shouldn't ever fail")
